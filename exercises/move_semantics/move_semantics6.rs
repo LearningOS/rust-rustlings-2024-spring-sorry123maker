be6e0563376ceffec8fb1bc5ea814ecd6b75f36b
sorry123maker <1830156635@qq.com>
2024-04-10 14:30:15 +0800

    update

--- exercises/move_semantics/move_semantics6.rs
+++ exercises/move_semantics/move_semantics6.rs
@@ -5,14 +5,14 @@
 // Execute `rustlings hint move_semantics6` or use the `hint` watch subcommand
 // for a hint.
 
-// I AM NOT DONE
+
 
 fn main() {
     let data = "Rust is great!".to_string();
 
+    let data1=data.clone();
     get_char(data);
-
-    string_uppercase(&data);
+    string_uppercase(&data1);
 }
 
 // Should not take ownership
@@ -22,7 +22,7 @@ fn get_char(data: String) -> char {
 
 // Should take ownership
 fn string_uppercase(mut data: &String) {
-    data = &data.to_uppercase();
+    data = data;
 
     println!("{}", data);
 }
